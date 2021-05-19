429f39de409f74834b4ae8ca58049e761deab640
hossam00 <hosamgamal33345@gmail.com>
2021-05-19 15:05:43 +0200

    edit the script to OOP

--- script.py
+++ script.py
@@ -1,14 +1,24 @@
 import requests
-import json
 
-url = "https://api.sofascore.com/api/v1/sport/football/events/live"
-response = requests.request("GET", url)
-json_data = json.loads(response.text)
+class sofaScoreScraper:
+    def __init__(self):
+        self.session = requests.session()
+    
+    def return_response(self):
+        url = "https://api.sofascore.com/api/v1/sport/football/events/live"
+        response = self.session.get(url).json()
+        return response
 
-for game in json_data['events']:
-    league = game['tournament']['name']
-    home_team = game['homeTeam']['name']
-    away_team = game['awayTeam']['name']
-    home_score = game['homeScore']['current']
-    away_score = game['awayScore']['current']
-    print(f'{league} | {home_team} {home_score} - {away_score} {away_team}')
+    def extract_data(self):
+        games = self.return_response()
+        for game in games['events']:
+            league = game['tournament']['name']
+            home_team = game['homeTeam']['name']
+            away_team = game['awayTeam']['name']
+            home_score = game['homeScore']['current']
+            away_score = game['awayScore']['current']
+            print(f'{league} | {home_team} {home_score} - {away_score} {away_team}')
+
+
+scraper = sofaScoreScraper()
+scraper.extract_data()
